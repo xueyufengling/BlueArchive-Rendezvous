4482801f53adc9b7352abcc91cf019ca6329d5bb
xueyufengling <923611078@qq.com>
2025-05-16 23:27:55 +0800

    dynamically load jar library class

--- src/main/java/ba/ModEntryObject.java
+++ src/main/java/ba/ModEntryObject.java
@@ -1,12 +1,18 @@
 package ba;
 
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import jvm.jar.JarFiles;
+import jvm.jar.JarKlassLoader;
 import jvm.klass.ObjectManipulator;
+import jvm.vm.VmManipulator;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.javafmlmod.FMLModContainer;
 
-@EventBusSubscriber(modid = ModEntryObject.ModId)
+//@EventBusSubscriber(modid = ModEntryObject.ModId)
 @Mod(value = ModEntryObject.ModId)
 public class ModEntryObject {
 	public static final String ModName = "BlueArchive: Rendezvous";
@@ -14,8 +20,28 @@ public class ModEntryObject {
 
 	public static final IEventBus ModBus = null;
 
-	public ModEntryObject(FMLModContainer container, IEventBus modBus) { 
-		ObjectManipulator.setObject(this.getClass(), "ModBus", modBus);// 设置ModBus
+	public static final Logger Logger = LogUtils.getLogger();
+
+	static {
+		loadLibrary();
 	}
 
+	public static void loadLibrary() {
+		JarKlassLoader.parentClassLoaderField = "fallbackClassLoader";// FML类加载器的默认父类加载器
+		JarKlassLoader.loadKlass("/libs/CommonUtils.jar");
+	}
+
+	public ModEntryObject(FMLModContainer container, IEventBus modBus) {
+		// 打印调试信息
+		Logger.info("Running on PID " + VmManipulator.getProcessId());
+		Logger.info("Mod located at " + JarFiles.getJarFilePath());
+		// 初始化赋值ModBus
+		ObjectManipulator.setObject(this.getClass(), "ModBus", modBus);
+
+		rmVanillaFeatures();
+	}
+
+	public static void rmVanillaFeatures() {
+		// RegistryFactory.DIMENSION_TYPE;
+	}
 }
--- src/main/java/ba/core/RegistryFactory.java
+++ src/main/java/ba/core/RegistryFactory.java
@@ -1,15 +1,29 @@
 package ba.core;
 
+import java.util.IdentityHashMap;
+
 import ba.ModEntryObject;
+import jvm.klass.ObjectManipulator;
 import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess.Frozen;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+@EventBusSubscriber(modid = ModEntryObject.ModId)
 public class RegistryFactory {
+	public static final Frozen registryAccess = null;
+
 	/**
 	 * 获取模组命名空间下的ResourceLocation
 	 * 
@@ -22,6 +36,7 @@ public class RegistryFactory {
 
 	public static final DeferredRegister<Block> BLOCK = deferredRegister(BuiltInRegistries.BLOCK);
 	public static final DeferredRegister<Item> ITEM = deferredRegister(BuiltInRegistries.ITEM);
+	//public static final DeferredRegister<DimensionType> DIMENSION_TYPE = deferredRegister(Registries.DIMENSION_TYPE);
 
 	/**
 	 * 获取注册表，该表里的条目会自动注册到ModBus
@@ -34,6 +49,10 @@ public class RegistryFactory {
 		DeferredRegister<T> deferredRegister = DeferredRegister.create(registry, ModEntryObject.ModId);
 		deferredRegister.register(ModEntryObject.ModBus);
 		return deferredRegister;
+	}
+
+	public static <T> DeferredRegister<T> deferredRegister(ResourceKey<? extends Registry<T>> resource_key) {
+		return deferredRegister(getRegistry(resource_key));
 	}
 
 	public static <R, T extends R> DeferredHolder<R, T> register(DeferredRegister<R> registry, String registerName, T obj) {
@@ -60,5 +79,46 @@ public class RegistryFactory {
 			}
 			return null;
 		});
+	}
+
+	/**
+	 * @param <T>
+	 * @param resource_key
+	 * @return
+	 */
+	public static <T> Registry<T> getRegistry(ResourceKey<? extends Registry<T>> resource_key) {
+		return registryAccess.registryOrThrow(resource_key);
+	}
+
+	/**
+	 * 解冻注册表冻结以注册，不记录入is_registrise_frozen，因此尽量不要使用
+	 * 
+	 * @return 操作是否成功
+	 */
+	public static <T> boolean unfreezeRegistry(Registry<T> registry) {
+		return ObjectManipulator.setBoolean(registry, "net.minecraft.core.MappedRegistry.frozen", false) && ObjectManipulator.setObject(registry, "net.minecraft.core.MappedRegistry.unregisteredIntrusiveHolders", new IdentityHashMap<>());
+	}
+
+	/**
+	 * 解冻注册表冻结以注册，并记录入is_registrise_frozen，应当总是使用该方法解冻注册表
+	 * 
+	 * @return 操作是否成功
+	 */
+	public static <T> boolean unfreezeRegistry(ResourceKey<? extends Registry<T>> resource_key) {
+		return unfreezeRegistry(getRegistry(resource_key));
+	}
+
+	/**
+	 * 冻结注册表，并记录入is_registrise_frozen
+	 * 
+	 * @return
+	 */
+	public static <T> void freezeRegistry(Registry<T> registry) {
+		registry.freeze();
+	}
+
+	@SubscribeEvent(priority = EventPriority.HIGHEST) // 最高优先级以获取注册表
+	public static void onServerStartup(ServerStartingEvent event) {
+		ObjectManipulator.setObject(RegistryFactory.class, "registryAccess", event.getServer().registryAccess());
 	}
 }
